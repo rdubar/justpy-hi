9c2dab7579f7f86a7cdd854794de21c649402fbd
rdubar <rdubar@gmail.com>
2021-05-24 10:45:18 +0100

    simple deployment

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import justpy as jp
 app = jp
 
-def hello_world():
+def app():
     wp = jp.WebPage()
     jp.Hello(a=wp)
     return wp
 
 def main():
-    jp.justpy(hello_world)
+    jp.justpy(app)
+    app = jp
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='127.0.0.1', port=8000, workers=8, debug=True)
